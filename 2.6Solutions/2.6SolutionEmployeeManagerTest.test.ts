a77d9a18f758f5177ff71a4c54fc7bb15851c9d8
Jeorgi Bernard <Jeorgi.Bernard@gmail.com>
2023-11-29 15:57:30 -0700

    2.7 homework push

---
+++ 2.6Solutions/2.6SolutionEmployeeManagerTest.test.ts
@@ -0,0 +1,51 @@
+import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
+  const chromedriver = require("chromedriver");
+
+  const driver: WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
+
+  class employeePage {
+      driver: WebDriver;
+      url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
+        //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
+    header: By = By.css('.titleText')
+    addEmployee: By = By.name('addEmployee')
+    newEmployee: By = By.xpath('//li[text()="New Employee"]')
+    nameInput: By = By.name('nameEntry')
+    phoneInput: By = By.name('phoneEntry')
+    titleInput: By = By.name('titleEntry')
+    //constructor
+    constructor(driver: WebDriver) {
+        this.driver = driver
+    }
+    //methods
+    async navigate() {
+        await this.driver.get(this.url); 
+        await this.driver.wait(until.elementLocated(this.header))
+    }
+  }
+const emPage = new employeePage(driver)
+  describe("Employee Manger Test", () => {
+      beforeEach(async () => {
+          await emPage.navigate();
+      })
+      afterAll(async () => {
+          await driver.quit()
+      })
+      test("adding an employee", async () => {
+          await driver.wait(until.elementLocated(emPage.header))
+          await driver.findElement(emPage.addEmployee).click()
+          await driver.findElement(emPage.newEmployee).click()
+          await driver.findElement(emPage.nameInput).click()
+          await driver.findElement(emPage.nameInput).clear()
+          await driver.findElement(emPage.nameInput).sendKeys("Change this")
+          await driver.findElement(emPage.phoneInput).click()
+          await driver.findElement(emPage.phoneInput).clear()
+          await driver.findElement(emPage.phoneInput).sendKeys("Change this")
+          await driver.findElement(emPage.titleInput).click()
+          await driver.findElement(emPage.titleInput).clear()
+          await driver.findElement(emPage.titleInput).sendKeys("Change this")
+  })
+  })
+  /* this is a commment */
